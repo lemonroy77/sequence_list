fefae8effbaedb1d4f52c3e143a499fdae580863
lemonroy77 <123099259+lemonroy77@users.noreply.github.com>
2023-08-12 15:37:25 +0800

    Add files via upload

---
+++ DeL_x.cpp
@@ -0,0 +1,25 @@
+/*.对长度为L的顺序表Z,编写一个时间复杂度为O(n)、空间复杂度为o(1)的算法，该算
+法删除线性表中所有值为x的数据元素。*/
+#include "sequlist.h"
+void del_x1(SeqList &L,int x){
+    int k=0,i;
+    for(i=0;i<L.length;i++)
+        if(L.data[i]!=x){
+            L.data[k]=L.data[i];
+            k++;
+        }
+    L.length=k;
+}
+int main(){
+        SeqList L;
+        int x;
+        input(L);
+        printf("原顺序表为：\n");
+        print(L);
+        printf("请输入要删除的元素：\n");
+        scanf("%d", &x);
+        del_x1(L, x);
+        printf("删除元素后的顺序表为：\n");
+        print(L);
+        return 0;
+}
